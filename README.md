7ad983f20276fc6da17c10d8c7792ca5bf3ad0e2
ntuzer <rgjr22@gmail.com>
2018-04-19 12:59:24 -0700

    Calibrate soil moisture sensor

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-#autogarden
+# autogarden
 
+This project aims to recreate a tropical environment that would enable tropical plants (ie. guanabana) to grow in non-tropical places. I plan to build a greenhouse environment using a small humidifer, a DHT11 thermo/hydro meter, a water pump, heater, a soil moisture sensor, a light sensor, and of course an Arduino.
--- soil/soil.ino
+++ soil/soil.ino
@@ -1,22 +1,5 @@
-/***************************************************
- This example reads Capacitive Soil Moisture Sensor.
- 
- Created 2015-10-21
- By berinie Chen <bernie.chen@dfrobot.com>
- 
- GNU Lesser General Public License.
- See <http://www.gnu.org/licenses/> for details.
- All above must be included in any redistribution
- ****************************************************/
-
-/***********Notice and Trouble shooting***************
- 1.Connection and Diagram can be found here: https://www.dfrobot.com/wiki/index.php?title=Capacitive_Soil_Moisture_Sensor_SKU:SEN0193
- 2.This code is tested on Arduino Uno.
- 3.Sensor is connect to Analog 0 port.
- ****************************************************/
-
-const int AirValue = 520;   //you need to replace this value with Value_1
-const int WaterValue = 260;  //you need to replace this value with Value_2
+const int AirValue = 518;   //you need to replace this value with Value_1
+const int WaterValue = 287;  //you need to replace this value with Value_2
 int intervals = (AirValue - WaterValue)/3;   
 int soilMoistureValue = 0;
 void setup() {
