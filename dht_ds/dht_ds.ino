b9577e50e9f7b751903ea9665fa9f1518a8023db
ntuzer <rgjr22@gmail.com>
2018-04-19 15:33:19 -0700

    Add test for combined temperature sensors

---
+++ dht_ds/dht_ds.ino
@@ -0,0 +1,48 @@
+#include<dht.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+dht DHT;
+#define DHT11_PIN 3
+#define ONE_WIRE_BUS 5
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+float Celcius=0;
+float Fahrenheit=0;
+
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+  Serial.println("DHT11");
+}
+
+
+void loop() { // READ DATA
+  sensors.requestTemperatures();
+  int chk = DHT.read11(DHT11_PIN);
+
+  if (digitalRead(0)>35){
+    digitalWrite(A3, HIGH);
+    delay(75000);
+    digitalWrite(A3, LOW);
+  }
+
+  Serial.println(" Humidity " );
+  Serial.println(DHT.humidity, 1);
+  Serial.println(" ");
+  Serial.println(" Temparature ");
+  Serial.println(DHT.temperature * 1.8 + 32, 1);
+  Serial.println("--------------------");
+  Celcius=sensors.getTempCByIndex(0);
+  Fahrenheit=sensors.toFahrenheit(Celcius);
+  Serial.print(" C  ");
+  Serial.print(Celcius);
+  Serial.print(" F  ");
+  Serial.println(Fahrenheit);
+  Serial.println("--------------------");
+  Serial.println(" ");
+  delay(2000);
+}
+
+//my
--- soil/soil.ino
+++ soil/soil.ino
@@ -1,23 +1,28 @@
 const int AirValue = 518;   //you need to replace this value with Value_1
 const int WaterValue = 287;  //you need to replace this value with Value_2
-int intervals = (AirValue - WaterValue)/3;   
+int intervals = (AirValue - WaterValue)/3;
 int soilMoistureValue = 0;
+
+// Dry: (520 430]
+// Wet: (430 350]
+// Water: (350 260]
+
+
 void setup() {
   Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
 }
+
 void loop() {
-soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
-if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals))
-{
-  Serial.println("Very Wet");
-}
-else if(soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals))
-{
-  Serial.println("Wet");
-}
-else if(soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals))
-{
-  Serial.println("Dry");
-}
-delay(100);
+  soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
+  Serial.println("---------------------------------");
+  Serial.println(soilMoistureValue);
+  if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals)){
+    Serial.println("Very Wet");
+  } else if(soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals)) {
+    Serial.println("Wet");
+  } else if(soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals)) {
+    Serial.println("Dry");
+  }
+
+  delay(100);
 }
